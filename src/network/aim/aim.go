9a804761bff58bbbfa9688785a206040e4baa11e
pinksub <subpurple_alt@outlook.com>
2023-01-08 01:27:21 -0500

    wip

--- src/network/aim/aim.go
+++ src/network/aim/aim.go
@@ -4,7 +4,7 @@ import (
 	"chimera/network"
 	"chimera/utility/logging"
 	"chimera/utility/tcp"
-	"encoding/hex"
+	"math/rand"
 )
 
 func LogonAIM() {
@@ -28,33 +28,50 @@ func LogonAIM() {
 				Connection: tcpServer,
 			}
 
-			// a FLAP header consists of the following:
+			// currently sequence and versionHandshaken are here, but they
+			// might move later
+			sequence := uint16(rand.Intn(0xFFFF))
+			versionHandshaken := false
 
-			// 2A - the marker
-			// 01 - the frame, in this case this frame means to initialize the connection and the data contains the server FLAP version
-			// which is a DWORD and always 1
-			// 0001 - the sequence, which is initialized to a random value between 0x0000 and 0xFFFF and it wraps to 0x0000 if it's 0xFFFF,
-			// otherwise increments for each packet sent to the client.
-			// 0004 - the length. i feel that this is self-explanatory.
-
-			// currently this is simply a example packet in hexadecimal which is decoded and sent to the client, but eventually I'll add a
-			// FLAP packet builder soon
-
-			data, err := hex.DecodeString("2A010001000400000001")
-			if err != nil {
-				return
+			versionFlap := FLAPPacket{
+				Frame:    FrameSignOn,
+				Sequence: sequence,
+				Data:     []byte{0x00, 0x00, 0x00, 0x01},
 			}
 
-			client.Connection.BinaryWriteTraffic(data)
+			client.Connection.BinaryWriteTraffic(FLAPSerialize(versionFlap))
 
 			for {
-				_, err := client.Connection.BinaryReadTraffic()
+				combined, err := client.Connection.BinaryReadTraffic()
 
 				if err != nil {
 					break
 				}
 
-				// TODO: parse the packets, add structures, etc.
+				packets, hasErr := FLAPDeserialize(combined)
+				if hasErr {
+					break
+				}
+
+				for _, packet := range packets {
+					if packet.Frame != FrameSignOn && !versionHandshaken {
+						return
+					}
+
+					switch packet.Frame {
+					case FrameSignOn:
+						versionHandshaken = true
+
+					case FrameData:
+						// SNAC data frame
+
+					case FrameError:
+						// Error frame
+
+					case FrameSignOff:
+						// Sign off frame
+					}
+				}
 			}
 		}()
 	}
---
+++ src/network/aim/flap.go
@@ -0,0 +1,62 @@
+package aim
+
+import (
+	"encoding/binary"
+)
+
+const (
+	FrameSignOn  = 0x01
+	FrameData    = 0x02
+	FrameError   = 0x03
+	FrameSignOff = 0x04
+)
+
+type FLAPPacket struct {
+	Frame    byte
+	Sequence uint16
+	Data     []byte
+}
+
+func FLAPSerialize(packet FLAPPacket) []byte {
+	flap := make([]byte, 6+len(packet.Data))
+
+	flap[0] = 0x2A
+	flap[1] = packet.Frame
+
+	// I'm so glad I can use slicing here
+	binary.BigEndian.PutUint16(flap[2:4], packet.Sequence)
+	binary.BigEndian.PutUint16(flap[4:6], uint16(len(packet.Data)))
+
+	copy(flap[6:], packet.Data)
+	return flap
+}
+
+func FLAPDeserialize(flap []byte) (outPackets []FLAPPacket, hasErr bool) {
+	packets := []FLAPPacket{}
+
+	i := 0
+
+	for i < len(flap) {
+		if flap[0] != 0x2A || len(flap)-i < 6 {
+			return packets, true
+		}
+
+		length := binary.BigEndian.Uint16(flap[i+4 : i+6])
+		packet := FLAPPacket{
+			Frame:    flap[i+1],
+			Sequence: binary.BigEndian.Uint16(flap[i+2 : i+4]),
+		}
+
+		if packet.Frame != FrameSignOn && packet.Frame != FrameData && packet.Frame != FrameError && packet.Frame != FrameSignOff {
+			return packets, true
+		}
+
+		packet.Data = make([]byte, length)
+		copy(packet.Data, flap[i+6:i+6+int(length)])
+
+		i += 6 + int(length)
+		packets = append(packets, packet)
+	}
+
+	return packets, false
+}
