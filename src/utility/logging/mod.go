971893d69a293a9b19fb19833caf171a34450a40
Luna <lu.raine@outlook.jp>
2023-01-08 15:17:50 +0100

    right align message type in log

--- src/utility/logging/mod.go
+++ src/utility/logging/mod.go
@@ -8,27 +8,27 @@ func internal(prefix string, suffix string, text string, format ...any) {
 }
 
 func Info(suffix string, text string, format ...any) {
-	internal("\033[32mINFO  \033[0m", suffix, text, format...)
+	internal("\033[32m  INFO\033[0m", suffix, text, format...)
 }
 
 func Warn(suffix string, text string, format ...any) {
-	internal("\033[33mWARN  \033[0m", suffix, text, format...)
+	internal("\033[33m  WARN\033[0m", suffix, text, format...)
 }
 
 func Error(suffix string, text string, format ...any) {
-	internal("\033[31mERROR \033[0m", suffix, text, format...)
+	internal("\033[31m ERROR\033[0m", suffix, text, format...)
 }
 
 func Fatal(suffix string, text string, format ...any) {
-	internal("\033[38;5;89mFATAL \033[0m", suffix, text, format...)
+	internal("\033[38;5;89m FATAL\033[0m", suffix, text, format...)
 }
 
 func Trace(suffix string, text string, format ...any) {
-	internal("\033[36mTRACE \033[0m", suffix, text, format...)
+	internal("\033[36m TRACE\033[0m", suffix, text, format...)
 }
 
 func Debug(suffix string, text string, format ...any) {
-	internal("\033[38;5;99mDEBUG \033[0m", suffix, text, format...)
+	internal("\033[38;5;99m DEBUG\033[0m", suffix, text, format...)
 }
 
 func System(suffix string, text string, format ...any) {
